ac5fcea699ddbe8d647eb3dc8032c0121b462b08
liudongbaollz <liudongbaollz@gmail.com>
2012-02-06 14:18:22 +0000

    已移植Tiles类到TilesGame类，但是显示的图形与box2d中的内容差别较大，尚需要定位后并修改。

--- box2dforandroid/mysrc/com/hellogame/TilesGame.java
+++ box2dforandroid/mysrc/com/hellogame/TilesGame.java
@@ -19,9 +19,9 @@ public class TilesGame extends Box2DTestGame {
 		super.create();
 		// Ground
 		createGroundTypes();
-
+		createTestTypes();
+		m_createTime = System.currentTimeMillis();
 	}
-
 	private void createGroundTypes() {
 		float a = 0.5f;
 		BodyDef groundBodyDef = new BodyDef();
@@ -43,11 +43,40 @@ public class TilesGame extends Box2DTestGame {
 			position.y -= 2.0f * a;
 		}
 	}
+	private void createTestTypes() {
+		{
+			float a = 0.5f;
+			PolygonShape shape = new PolygonShape();
+			shape.setAsBox(a, a);
+
+			Vector2 x = new Vector2(-7.0f, 0.75f);
+			Vector2 y = null;
+			Vector2 deltaX = new Vector2(0.5625f, 1.25f);
+			Vector2 deltaY = new Vector2(1.125f, 0.0f);
+
+			for (int i = 0; i < 20; ++i) {
+				y = x;
+
+				for (int j = i; j < 20; ++j) {
+					BodyDef bd = new BodyDef();
+					bd.type = BodyType.DynamicBody;
+					bd.position.set(y);
+					Body body = world.createBody(bd);
+					body.createFixture(shape, 5.0f);
+					++m_fixtureCount;
+					y.add(deltaY);
+				}
+
+				x.add(deltaX);
+			}
+		}
+	}
+
+
 
 	@Override
 	protected void step() {
 		super.step();
-		
 
 	}
 }
