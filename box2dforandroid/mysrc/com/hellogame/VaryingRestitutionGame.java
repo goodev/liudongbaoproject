257c1931c3cf0ce92ba34a99b9f5983f7ca2eecf
liudongbaollz <liudongbaollz@gmail.com>
2012-02-08 14:07:37 +0000

    移植VaryingRestitution类到VaryingRestitutionGame类，演示恢复(restitution)。 (译注:一个物体和另一物体碰撞, 碰撞后速度和碰撞前速度的比值会保持不变，这比值就叫恢复系数。)

---
+++ box2dforandroid/mysrc/com/hellogame/VaryingRestitutionGame.java
@@ -0,0 +1,43 @@
+package com.hellogame;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+public class VaryingRestitutionGame extends Box2DTestGame {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		super.create();
+		// Ground
+		BodyDef groundBodyDef = new BodyDef();
+		groundBody = world.createBody(groundBodyDef);
+		EdgeShape edgeShape = new EdgeShape();
+		edgeShape.set(new Vector2(-40.0f, 0.0f), new Vector2(40.0f, 0.0f));
+		groundBody.createFixture(edgeShape, 0.0f);
+
+		{
+			CircleShape shape = new CircleShape();
+			shape.setRadius(1.0f);
+			FixtureDef fd = new FixtureDef();
+			fd.shape = shape;
+			fd.density = 1.0f;
+
+			float[] restitution = { 0.0f, 0.1f, 0.3f, 0.5f, 0.75f, 0.9f, 1.0f };
+			for (int i = 0; i < 7; ++i) {
+				BodyDef bd = new BodyDef();
+				bd.type = BodyType.DynamicBody;
+				bd.position.set(-10.0f + 3.0f * i, 20.0f);
+				Body body = world.createBody(bd);
+				fd.restitution = restitution[i];
+				body.createFixture(fd);
+			}
+		}
+	}
+
+}
