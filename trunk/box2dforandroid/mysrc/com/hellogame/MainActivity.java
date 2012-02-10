694385d6deac3979c3ed55901b2b948f92826ef2
liudongbaollz@gmail.com <liudongbaollz@gmail.com@bdec6a22-31ca-571c-bda5-32aa072b72c7>
2012-02-10 14:13:58 +0000

    完成移植BodyTypes类到BodyTypesGame类

--- trunk/box2dforandroid/mysrc/com/hellogame/MainActivity.java
+++ trunk/box2dforandroid/mysrc/com/hellogame/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AndroidApplication {
 
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
-		initialize(new EdgeTestGame(), false);
+		initialize(new BodyTypesGame(), false);
 	}
 }
