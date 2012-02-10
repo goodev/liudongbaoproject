488bb66bb0c257399e25a0c56550c46f466e3ab8
liudongbaollz@gmail.com <liudongbaollz@gmail.com@bdec6a22-31ca-571c-bda5-32aa072b72c7>
2012-02-10 14:31:46 +0000

    完成移植ShapeEditing类到ShapeEditingGame类

---
+++ trunk/box2dforandroid/mysrc/com/hellogame/ShapeEditingGame.java
@@ -0,0 +1,78 @@
+package com.hellogame;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+public class ShapeEditingGame extends Box2DTestGame {
+
+	Body m_body;
+	Fixture m_fixture1;
+	Fixture m_fixture2;
+	boolean m_sensor;
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		super.create();
+		{
+			BodyDef bd = new BodyDef();
+			Body ground = world.createBody(bd);
+
+			EdgeShape shape = new EdgeShape();
+			shape.set(new Vector2(-40.0f, 0.0f), new Vector2(40.0f, 0.0f));
+
+			ground.createFixture(shape, 0.0f);
+		}
+
+		BodyDef bd = new BodyDef();
+		bd.type = BodyType.DynamicBody;
+		bd.position.set(0.0f, 10.0f);
+		m_body = world.createBody(bd);
+
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(4.0f, 4.0f, new Vector2(0.0f, 0.0f), 0.0f);
+		m_fixture1 = m_body.createFixture(shape, 10.0f);
+
+		m_fixture2 = null;
+
+		m_sensor = false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		switch (character) {
+		case 'c':
+			if (m_fixture2 == null) {
+				CircleShape shape = new CircleShape();
+				shape.setRadius(3.0f);
+				shape.setPosition(new Vector2(0.5f, -4.0f));
+				m_fixture2 = m_body.createFixture(shape, 10.0f);
+				m_body.setAwake(true);
+			}
+			break;
+
+		case 'd':
+			if (m_fixture2 != null) {
+				m_body.destroyFixture(m_fixture2);
+				m_fixture2 = null;
+				m_body.setAwake(true);
+			}
+			break;
+
+		case 's':
+			if (m_fixture2 != null) {
+				m_sensor = !m_sensor;
+				m_fixture2.setSensor(m_sensor);
+			}
+			break;
+		}
+		return super.keyTyped(character);
+	}
+
+}
