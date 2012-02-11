08a8d8553da2d8cc3f89fbf7eb1f280e762b4202
liudongbaollz <liudongbaollz@gmail.com>
2012-02-11 01:02:46 +0000

    增加EnterGameList 类用于展示demo入口类

--- box2dforandroid/mysrc/com/hellogame/MainActivity.java
+++ box2dforandroid/mysrc/com/hellogame/MainActivity.java
@@ -1,13 +1,23 @@
 package com.hellogame;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
 public class MainActivity extends AndroidApplication {
 
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
-		initialize(new ShapeEditingGame(), false);
+		String ApplicationListener = getIntent().getExtras().getString(
+				"ApplicationListener");
+		try {
+			initialize((ApplicationListener) Class.forName(ApplicationListener)
+					.newInstance(), false);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(null, e.getMessage(), e);
+		}
 	}
 }
